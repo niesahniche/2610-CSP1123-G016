eb72aad69e6cffdae59c9d121b9cff28d4878bce
Nadhrah Ibtisam <nad125sam@gmail.com>
2026-04-24 20:32:51 +0800

    grocery Ali can be compared to recipe.id 1 (integration)

--- campuscook/pages/urls.py
+++ campuscook/pages/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("grocery/", views.grocery, name="grocery"),
+    path("check/<int:recipe_id>/", views.check_recipe, name="check_recipe"),
+    path('remove/<int:id>/', views.remove_item, name='remove_item'),
     
 ]
 
--- campuscook/pages/views.py
+++ campuscook/pages/views.py
@@ -13,7 +13,7 @@ def about(request):
 #paste your all views here
 from django.shortcuts import render
 from django.shortcuts import redirect
-from .models import Grocery, AppUser , Ingredient
+from .models import Grocery, AppUser , Ingredient , Recipe
 
 #grocery list view
 def grocery(request):
@@ -45,3 +45,54 @@ def grocery(request):
         'ingredients': ingredients
     })
     
+
+#grocery list view
+def grocery(request):
+    user = AppUser.objects.first()  # temporary user
+
+    if request.method == "POST":
+
+        # Add preset ingredient
+        if 'ingredient_id' in request.POST:
+            ing_id = request.POST.get('ingredient_id')
+            ingredient = Ingredient.objects.get(id=ing_id)
+
+            Grocery.objects.create(user=user, ingredient=ingredient)
+
+        # Add custom ingredient
+        elif 'custom_name' in request.POST:
+            name = request.POST.get('custom_name')
+
+            if name:
+                Grocery.objects.create(user=user, custom_name=name)
+
+        return redirect('grocery')
+
+    groceries = Grocery.objects.filter(user=user)
+    ingredients = Ingredient.objects.all()
+
+    return render(request, 'pages/grocery.html', {
+        'groceries': groceries,
+        'ingredients': ingredients
+    })
+    
+
+def remove_item(request, id):
+    Grocery.objects.filter(id=id).delete()
+    return redirect('grocery')
+    
+
+def check_recipe(request, recipe_id): 
+    user = AppUser.objects.first()  # temporary user
+    
+    recipe = Recipe.objects.get(id=recipe_id)
+    recipe_ingredients = recipe.ingredients.all()
+    
+    my_groceries = Ingredient.objects.filter(grocery__user=user)
+
+    missing = recipe_ingredients.exclude(id__in=my_groceries)
+
+    return render(request, 'pages/check.html', {
+        'recipe': recipe,
+        'missing': missing
+    })
